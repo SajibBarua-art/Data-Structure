cb45368c6067795e7ec7ad2f6a8cfaaeea22188e
Sajib Barua <73757389+SajibBarua-art@users.noreply.github.com>
2023-02-04 13:32:07 +0600

    Update 02. Lazy with propagation.cpp

--- Segment Tree/02. Lazy with propagation.cpp
+++ Segment Tree/02. Lazy with propagation.cpp
@@ -1,93 +1,135 @@
 // You are given some ranges, you have to increase the range by 'val'.
 // In a query you are able to know the sum of a range. 
+// o based indexing
 
 #include<bits/stdc++.h>
 using namespace std;
-const int maxN = 2e5+5;
-int arr[maxN];
-int segmentTree[4*maxN], lazy[4*maxN];
+const int maxN = 2e5+50;
 
-void buildTree(int sIndex, int sStart, int sEnd) {
-    if(sStart == sEnd) {
-        segmentTree[sIndex] = arr[sStart];
-        return;
+int tree[4*maxN] = {0};  // To store segment tree
+int lazy[4*maxN] = {0};  // To store pending updates
+int arr[maxN]; // To store the given array which you may update
+
+// To handle testcases
+void reload(int n) {
+    for(int i=0; i<=4*n; i++) {
+        tree[i] = 0;
+        lazy[i] = 0;
+        arr[i] = 0;
     }
-
-    int mid = (sStart+sEnd)/2;
-    buildTree(sIndex*2, sStart, mid);
-    buildTree(sIndex*2+1, mid+1, sEnd);
-
-    segmentTree[sIndex] = segmentTree[sIndex*2] + segmentTree[sIndex*2+1];
 }
 
-int query(int sIndex, int sStart, int sEnd, int qStart, int qEnd) {
-    // for pending update
-    if(lazy[sIndex] != 0) {
-        int dx = lazy[sIndex];
-        lazy[sIndex] = 0;
-        segmentTree[sIndex] += (dx*(sEnd-sStart));
-
-        if(sStart != sEnd) {
-            lazy[sIndex*2] += dx;
-            lazy[sIndex*2 + 1] += dx;
+void updateRangeUtil(int si, int ss, int se, int us,
+                     int ue, int diff)
+{
+    if (lazy[si] != 0)
+    {
+        tree[si] += (se-ss+1)*lazy[si];
+        if (ss != se)
+        {
+            lazy[si*2 + 1]   += lazy[si];
+            lazy[si*2 + 2]   += lazy[si];
         }
+  
+        lazy[si] = 0;
     }
 
-    if(qStart>sEnd || qEnd<sStart) // completely outside
-        return 0; // change here
-    if(sStart>=qStart && sEnd<=qEnd) // completely inside
-        return segmentTree[sIndex];
-
-    int mid = (sStart + sEnd)/2;
-    int l = query(sIndex*2, sStart, mid, qStart, qEnd);
-    int r = query(sIndex*2+1, mid+1, sEnd, qStart, qEnd);
-    return (l+r); // change here
-}
-
-void update(int sIndex, int sStart, int sEnd, int qStart, int qEnd, int val) {
-    // for pending update
-    if(lazy[sIndex] != 0) {
-        int dx = lazy[sIndex];
-        lazy[sIndex] = 0;
-        segmentTree[sIndex] += (dx*(sEnd-sStart));
-
-        if(sStart != sEnd) {
-            lazy[sIndex*2] += dx;
-            lazy[sIndex*2 + 1] += dx;
-        }
-    }
-
-    if(sStart>qEnd || sEnd<qStart) return;
-    if(sStart>=qStart && sEnd<=qEnd) {
-        int dx = (sEnd-sStart+1)*val;
-        segmentTree[sIndex] += dx;
-
-        if(sStart != sEnd) {
-            lazy[2*sIndex] += val;
-            lazy[2*sIndex+1] += val;
+    if (ss>se || ss>ue || se<us)
+        return ;
+    if (ss>=us && se<=ue)
+    {
+        tree[si] += (se-ss+1)*diff;
+        if (ss != se)
+        {
+            lazy[si*2 + 1]   += diff;
+            lazy[si*2 + 2]   += diff;
         }
         return;
     }
-
-    int mid = (sStart + sEnd)/2;
-    update(sIndex*2, sStart, mid, qStart, qEnd, val);
-    update(sIndex*2+1, mid+1, sEnd, qStart, qEnd, val);
-
-    segmentTree[sIndex] = segmentTree[sIndex*2] + segmentTree[sIndex*2+1]; // change here
+    int mid = (ss+se)/2;
+    updateRangeUtil(si*2+1, ss, mid, us, ue, diff);
+    updateRangeUtil(si*2+2, mid+1, se, us, ue, diff);
+  
+    tree[si] = tree[si*2+1] + tree[si*2+2];
 }
 
-int main() {
-    int n, q, code, l, r, value; cin >> n >> q;
-    buildTree(1, 1, n);
-    while(q--) {
-        cin >> code;
-        if(code == 1) {
-            cin >> l >> r;
-            cout << query(1, 1, n, l, r) << endl;
+void updateRange(int n, int us, int ue, int diff)
+{
+   updateRangeUtil(0, 0, n-1, us, ue, diff);
+}
+int getSumUtil(int ss, int se, int qs, int qe, int si)
+{
+    if (lazy[si] != 0)
+    {
+        tree[si] += (se-ss+1)*lazy[si];
+  
+        if (ss != se)
+        {
+            lazy[si*2+1] += lazy[si];
+            lazy[si*2+2] += lazy[si];
         }
-        else {
-            cin >> l >> r >> value;
-            update(1, 1, n, l, r, value);
-        }
+        lazy[si] = 0;
     }
+    if (ss>se || ss>qe || se<qs)
+        return 0;
+    if (ss>=qs && se<=qe)
+        return tree[si];
+
+    int mid = (ss + se)/2;
+    return getSumUtil(ss, mid, qs, qe, 2*si+1) +
+           getSumUtil(mid+1, se, qs, qe, 2*si+2);
+}
+int getSum(int n, int qs, int qe)
+{
+    if (qs < 0 || qe > n-1 || qs > qe)
+    {
+        printf("Invalid Input");
+        return -1;
+    }
+  
+    return getSumUtil(0, n-1, qs, qe, 0);
+}
+void constructSTUtil(int ss, int se, int si)
+{
+    if (ss > se)
+        return ;
+  
+    if (ss == se)
+    {
+        tree[si] = arr[ss];
+        return;
+    }
+  
+    int mid = (ss + se)/2;
+    constructSTUtil(ss, mid, si*2+1);
+    constructSTUtil(mid+1, se, si*2+2);
+  
+    tree[si] = tree[si*2 + 1] + tree[si*2 + 2];
+}
+
+void constructST(int n)
+{
+    constructSTUtil(0, n-1, 0);
+}
+
+int main()
+{
+    arr[] = {1, 3, 5, 7, 9, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+  
+    // Build segment tree from given array
+    constructST(n);
+  
+    // Print sum of values in array from index 1 to 3
+    printf("Sum of values in given range = %d\n",
+           getSum(n, 1, 3));
+  
+    // Add 10 to all nodes at indexes from 1 to 5.
+    updateRange(n, 1, 5, 10);
+  
+    // Find sum after the value is updated
+    printf("Updated sum of values in given range = %d\n",
+            getSum(n, 1, 3));
+  
+    return 0;
 }
